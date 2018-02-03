c2a7a9997be7958be74e466dcc722454c16e9819
DESKTOP-0ONJAOC\Danny <dannydejonge1710@gmail.com>
2018-02-03 16:50:24 +0100

    no message

--- index.php
+++ index.php
@@ -1,55 +1,46 @@
+<?php
+
+	date_default_timezone_set("Europe/Amsterdam");
+	echo "<h1>Goede";
+
+	if (date("H") <6) {
+		echo " nacht";
+		$time = "img/night.png" ;
+		$color = red ;	
+	} 
+		
+	else if (date("H") < 12) {
+		echo " morgen";
+		$time = "img/morning.png" ;	
+		$color = green ;		
+	} 
+
+	else if (date("H") < 18) {
+		echo " middag";
+		$time = "img/afternoon.png" ;
+		$color = yellow ;			
+	}
+
+	else if (date("H") < 24) {
+		echo " avond";
+		$time = "img/evening.png" ;	
+		$color = white ;		
+	}
+
+	echo "<br>Het is nu " . date("H:i") . "</h1>" ;	
+?>
+
 <!DOCTYPE html>
 
 <html>
 <head>
+	<style><?php include 'css/style.css'; ?></style>
 	<title>Goedemorgen</title>
-	<link rel="stylesheet" type="text/css" href="css.css" />
 </head>
+
 <body>
 
-	<?php
-
-		date_default_timezone_set("Europe/Amsterdam");
-		echo "<h1>Goeden";
-
-		if (date("H") <6) {
-			echo " nacht</h1>";
-			$time = "night.png" ;
-			
-		} else
-
-		if (date("H") < 12) {
-			echo " morgen</h1>";
-			$time = "morning.png" ;
-						
-		} else
-
-		if (date("H") < 18) {
-			echo " middag</h1>";
-			$time = "afternoon.png" ;
-					
-		} else
-
-		if (date("H") < 24) {
-			echo " avond</h1>";
-			$time = "evening.png" ;
-					
-		} else 
-
-		echo "<br><h2>Het is nu " . date("H:i</h2>");	
-	?>
-	
-	<main>
-		<style>
-
-			body{
-				background-image: url(<?php echo $time ?>);
-				background-size: cover;
-				background-repeat: no-repeat;
-			}
-
-		</style>
-	</main>
 
 </body>
+
 </html>
